12f89281ee43a4a85cc106529a19190c100a071a
Patrick Glendon McCullough <p@mccullo.ug>
2024-04-17 15:47:13 -0400

    it flashes

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const app = document.getElementById('app');
 
 app.innerText = "Loading...";
 app.style.color = "green";
+
+setInterval(() => {
+  if(app.style.opacity !== 1) {
+    app.style.opacity = 0;
+  } else {
+    app.style.opacity = 1;
+  }
+}, 500);
