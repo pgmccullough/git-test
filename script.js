7eee7a6a28f0be2ed4cc9fcaab39747d296713ad
Patrick Glendon McCullough <p@mccullo.ug>
2024-04-17 15:53:30 -0400

    fix not flashing bug

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const app = document.getElementById('app');
 app.innerText = "Loading...";
 app.style.color = "green";
 
+let isOn = true
+
 setInterval(() => {
-  if(app.style.opacity !== 1) {
+  if(isOn) {
     app.style.opacity = 0;
+    isOn = false;
   } else {
     app.style.opacity = 1;
+    isOn = true;
   }
 }, 500);
